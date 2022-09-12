1c71ef379f3cbe7b6e4423babbc8e77964829f1e
idmed22 <idlmhor.med@gmail.com>
2022-09-12 12:09:21 +0100

    first commit

---
+++ Souss_Health_Brief16/src/main/java/com/Souss_Health_Brief16/SoussHealthBrief16Application.java
@@ -0,0 +1,13 @@
+package com.Souss_Health_Brief16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoussHealthBrief16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoussHealthBrief16Application.class, args);
+	}
+
+}
---
+++ Souss_Health_Brief16/src/test/java/com/Souss_Health_Brief16/SoussHealthBrief16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Souss_Health_Brief16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoussHealthBrief16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
